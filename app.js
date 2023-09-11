f1b4ff1055b53e38dbc18f1415a55b31034f1f62
Carlos Ronny de Sousa <135059087+prof-ronny@users.noreply.github.com>
2023-09-11 16:11:34 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mysql = require('mysql2/promise');
 
 const app = express();
-const port = $PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
