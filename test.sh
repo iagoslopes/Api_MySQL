2495c56178ed64ba638bc54b3540da0b831fe44b
Carlos Ronny de Sousa <135059087+prof-ronny@users.noreply.github.com>
2023-09-11 14:54:42 -0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -29,7 +29,6 @@ if [ "$response" -eq 200 ]; then
   echo "A API está funcionando corretamente (código de resposta: $response)."
 else
   echo "A API não está funcionando corretamente (código de resposta: $response)."
+  return 1
 fi
 
-# Encerre o aplicativo Docker Compose
-docker-compose down
